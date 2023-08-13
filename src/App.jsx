e632c9ab7c57a8bde7a20b6760c97eb7c8cef763
İsa Can Çabuk <isacancabuk@gmail.com>
2023-08-13 23:32:08 +0300

    initial array is deleted

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,6 @@ import Recipes from "./components/Recipes/Recipes";
 import NewRecipe from "./components/NewRecipe/NewRecipe";
 import EditRecipe from "./components/EditRecipe/EditRecipe";
 
-const initRecipes = [
-  {
-    id: "e2",
-    imageUrl:
-      "https://images.unsplash.com/photo-1644405636176-ee3f6ebd891e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=989&q=80",
-    recipeName: "Recipe2",
-    recipeDescription: "RecipeDescription2",
-  },
-  {
-    id: "e3",
-    imageUrl:
-      "https://images.unsplash.com/photo-1636055608368-da999007dbd6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=988&q=80",
-    recipeName: "Recipe3",
-    recipeDescription: "RecipeDescription3",
-  },
-];
 const initData = {
   state: false,
   selectedIndex: -1,
